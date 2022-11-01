cbd98ae76d156763d3e6861d5a264ac95769403a
tmsnvl95 <tmsnvl95@naver.com>
2022-11-01 16:49:22 +0900

    products_view_review_index

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+    product = Product.objects.order_by("-pk")
+    context = {
+        "product": product,
+    }
+    return render(request, "products/index.html", context)
+
+
