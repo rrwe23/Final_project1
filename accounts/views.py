57d23897bb546fcb02b55b9e8614e8a750a4a8a7
qlghwp123 <qlghwp123@naver.com>
2022-11-01 14:49:33 +0900

    Add : accounts/login and url to base.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def signup(request):
@@ -12,6 +13,7 @@ def signup(request):
             # user = form.save()
             # auth_login(request, user)
 
+            # 수정 필요
             return redirect('accounts:signup')
     else:
         form = CustomUserCreationForm()
@@ -21,3 +23,23 @@ def signup(request):
     }
 
     return render(request, 'accounts/signup.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            auth_login(request, form.get_user())
+
+            # 수정 필요
+            return redirect(request.GET.get('next') or 'accounts:signup')
+
+    else:
+        form = AuthenticationForm(request)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'accounts/login.html', context)
