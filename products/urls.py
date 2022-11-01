1ca9f6ae319b2c08f85716b72141698741a71b9a
qlghwp123 <qlghwp123@naver.com>
2022-11-01 11:53:44 +0900

    Add : first setting

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+app_name = 'products'
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # path('accounts/', include('accounts.urls')),
+    # path('products/', include('products.urls')),
+    # path('reviews/', include('reviews.urls')),
+]
