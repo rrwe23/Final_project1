2289c5238fb34fb357b226239fda04bdb859fde7
rrwe23 <jhrrt0626@gmail.com>
2022-11-01 16:28:51 +0900

    1101 models

--- pjt/settings.py
+++ pjt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'imagekit',
     'accounts',
     'products',
     'django_bootstrap5',
--- products/models.py
+++ products/models.py
@@ -1,3 +1,57 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+
+
+
+class Product(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    thumbnail = ProcessedImageField(upload_to="images/", blank=True,
+                                    processors=[ResizeToFill(300,200)],
+                                    format ='JPEG',
+                                    options = {'quality':80})
+    image = ProcessedImageField(upload_to='images/', blank=True,
+                                    processors=[ResizeToFill(1200,960)],
+                                    format='JPEG',
+                                    options={'quality':80})
+    content = models.TextField()
+    cart = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'cart_product')
+    review = models.ManyToManyField('products.Review', related_name = 'review_product')
+    qna = models.ManyToManyField('products.Question', related_name = 'qna_product')
+
+
+                    
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    star = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add = True)
+    image = ProcessedImageField(upload_to="images/", blank=True,
+                                    processors=[ResizeToFill(200,100)],
+                                    format ='JPEG',
+                                    options = {'quality':80})
+    content = models.TextField()
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'like_reviews')
+
+
+class Question(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    content = models.TextField()
+    answers = models.OneToOneField('products.Answer', on_delete = models.CASCADE)
+
+class Answer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    content = models.TextField()
+    questions = models.OneToOneField('products.Question', on_delete = models.CASCADE)
+
+
+
+
