495cdb652b5b8a97c4a383954a46e59c97066081
rrwe23 <jhrrt0626@gmail.com>
2022-11-01 16:47:30 +0900

    folder

--- products/urls.py
+++ products/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # path("<int:product_pk>/cart/", views.cart, name="cart"), # 장바구니 담기
 
     # # 리뷰
-    # path("<int:product_pk>/review/", views.review_index, name="review_index"), # 리뷰 목록 리스트
+    path("<int:product_pk>/review/", views.review_index, name="review_index"), # 리뷰 목록 리스트
     # path("<int:product_pk>/review/create", views.review_create, name="review_create"), # 상품 리뷰 작성
     # path("<int:product_pk>/review/<int:review_pk>/update", views.review_update, name="review_update"), # 상품 리뷰 수정
     # path("<int:product_pk>/review/<int:review_pk>/delete", views.review_delete, name="review_delete"), # 상품 리뷰 삭제
