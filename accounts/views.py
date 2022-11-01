d8a757ccc7e3d317850d9ac716e71ec3956c9398
qlghwp123 <qlghwp123@naver.com>
2022-11-01 14:31:56 +0900

    Add : accounts/signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # user = form.save()
+            # auth_login(request, user)
+
+            return redirect('accounts:signup')
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'accounts/signup.html', context)
