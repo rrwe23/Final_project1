794314f86f06e712917b35c7dc8701e34f72ccb0
rrwe23 <jhrrt0626@gmail.com>
2022-11-02 17:24:42 +0900

    review_index

--- pjt/urls.py
+++ pjt/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
+    path('reviews/', include('reviews.urls')),
+    path('qna/', include('qna.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- qna/urls.py
+++ qna/urls.py
@@ -6,9 +6,9 @@ app_name = 'qna'
 urlpatterns = [
 
     # Q&A
-    path("<int:user_pk>/<int:product_pk>/qna/", views.qna_index, name="qna_index"), # qna 리스트
-    path("<int:user_pk>/<int:product_pk>/qna/create/question", views.question_create, name="question_create"), # 상품 문의 작성
-    path("<int:user_pk>/<int:product_pk>/qna/create/answer", views.answer_create, name="answer_create"), # 상품 답변 작성
-    path("<int:user_pk>/<int:product_pk>/qna/<int:qna_pk>/update", views.qna_update, name="qna_update"), # 상품 문의 수정
-    path("<int:user_pk>/<int:product_pk>/qna/<int:qna_pk>/delete", views.qna_delete, name="qna_delete"), # 상품 문의 삭제
+    # path("<int:user_pk>/<int:product_pk>/qna/", views.qna_index, name="qna_index"), # qna 리스트
+    # path("<int:user_pk>/<int:product_pk>/qna/create/question", views.question_create, name="question_create"), # 상품 문의 작성
+    # path("<int:user_pk>/<int:product_pk>/qna/create/answer", views.answer_create, name="answer_create"), # 상품 답변 작성
+    # path("<int:user_pk>/<int:product_pk>/qna/<int:qna_pk>/update", views.qna_update, name="qna_update"), # 상품 문의 수정
+    # path("<int:user_pk>/<int:product_pk>/qna/<int:qna_pk>/delete", views.qna_delete, name="qna_delete"), # 상품 문의 삭제
 ]
