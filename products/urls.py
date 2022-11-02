4691180fd3dea71f735eef9d98f32a5cc466000b
tmsnvl95 <tmsnvl95@naver.com>
2022-11-02 09:56:04 +0900

    prducts_index.html

--- products/urls.py
+++ products/urls.py
@@ -7,13 +7,12 @@ urlpatterns = [
 
     # 상품
 
-    path("", views.index, name="index"), # 상점이 있는 메인 화면
+    path("", views.main, name="main"), # 상점이 있는 메인 화면
     path("<int:user_pk>", views.index, name="index"), # 상품 리스트
     path("<int:user_pk>/create/", views.create, name="create"), # 상품 글 작성
     path("<int:user_pk>/<int:product_pk>/", views.detail, name="detail"), # 상품 조회 페이지
     path("<int:user_pk>/<int:product_pk>/update/", views.update, name="update"), # 상품 글 수정
     path("<int:user_pk>/<int:product_pk>/delete/", views.delete, name="delete"), # 상품 글 삭제
     path("<int:user_pk>/<int:product_pk>/cart/", views.cart, name="cart"), # 장바구니 담기
-
    
 ]
