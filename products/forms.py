063699ec550ba62f7c5329517f8cc61f0e3005e1
tmsnvl95 <tmsnvl95@naver.com>
2022-11-02 11:15:24 +0900

    product_index

---
+++ products/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        field = ['name', 'price', 'thumbnail', 'image', 'content']
--- products/views.py
+++ products/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import get_user_model
 
-def index(request):
+
+def main(request):
+    sellers = get_user_model().objects.filter(is_seller=True)
+    context = {
+        "sellers": sellers
+    }
+    return render(request, "products/main.html", context)
+
+    
+
+def index(request, user_pk):
     product = Product.objects.order_by("-pk")
     context = {
         "product": product,
     }
     return render(request, "products/index.html", context)
+    
 
+
+def create(request):
+    pass
+
+
+   
+def detail(request):
+    pass
+
+def update(request):
+    pass
+
+def delete(request):
+    pass
+
+def cart(request):
+    pass
 
 
 
