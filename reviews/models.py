bd73e2f3aad6e6e684ec4569e08419f2fd2eb49e
qlghwp123 <qlghwp123@naver.com>
2022-11-03 11:17:45 +0900

    Fix : add reviews/update and delete qna stuff, images and update some fix

--- reviews/models.py
+++ reviews/models.py
@@ -16,9 +16,4 @@ class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     star = models.CharField(max_length=5, choices=star_list)
     created_at = models.DateTimeField(auto_now_add = True)
-    image = ProcessedImageField(upload_to="images/", blank=True,
-                                    processors=[ResizeToFill(200,100)],
-                                    format ='JPEG',
-                                    options = {'quality':80})
     content = models.TextField()
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'like_reviews')
