ee83d09c97538d387056975f482cf947d31d93cc
qlghwp123 <qlghwp123@naver.com>
2022-11-03 14:43:26 +0900

    Fix : delete follow field in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,3 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     is_seller = models.BooleanField(default=False)
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
